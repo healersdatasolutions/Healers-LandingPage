d8d0d068624bc0942e729de61cd36ff206ebdb85
healersdatasolutions <166136254+healersdatasolutions@users.noreply.github.com>
2024-10-09 19:23:39 +0530

    Update App.jsx
    app.jss updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,17 @@ import {
 
 import { ThemeProvider } from "../src/components/ThemeProvider/theme-provider"
 import LandingPage from "./pages/LandingPage/LandingPage";
-import Ehr from "./pages/EhrPage/EhrPage";
-import DoctorDashboard from "./pages/DoctorDashboard/DoctorDashboard";
-import InventoryPage from "./pages/Inventory/InventoryPage";
-import Appointments from "./pages/Appointments/Appointments";
-import PatientDetails from "./components/PatientRecord1";
-import MedicalHistory from "./components/MedicalHistory";
-import TestReport from "./components/TestReport";
-import DoctorAppointments from "./pages/DoctorPages/AppointmentsPage/DoctorAppointments";
+// import Ehr from "./pages/EhrPage/EhrPage";
+// import DoctorDashboard from "./pages/DoctorDashboard/DoctorDashboard";
+// import InventoryPage from "./pages/Inventory/InventoryPage";
+// import Appointments from "./pages/Appointments/Appointments";
+// import PatientDetails from "./components/PatientRecord1";
+// import MedicalHistory from "./components/MedicalHistory";
+// import TestReport from "./components/TestReport";
+// import DoctorAppointments from "./pages/DoctorPages/AppointmentsPage/DoctorAppointments";
 import LoginSignup from "./pages/Login-Signup/Login-Signup";
-import Dashboard from "./pages/DashboardPage/Dashboard";
+// import Dashboard from "./pages/DashboardPage/Dashboard";
+import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
 
 
 function App() {
@@ -26,49 +27,53 @@ function App() {
       path: "/",
       element: <LandingPage />,
     },
-    {
-      path: "/dashboard",
-      element: <Dashboard />,
-    },
+    // {
+    //   path: "/dashboard",
+    //   element: <Dashboard />,
+    // },
     // other pages....
     {
       path: "/login-signup",
       element: <LoginSignup />,
 
     },
-    {
-      path: "/health-records",
-      element: <Ehr />,
-    },
-    {
-      path: "/doctor-dashboard",
-      element: <DoctorDashboard />,
-    },
+    // {
+    //   path: "/health-records",
+    //   element: <Ehr />,
+    // },
+    // {
+    //   path: "/doctor-dashboard",
+    //   element: <DoctorDashboard />,
+    // },
 
-    {
-      path: "/inventory",
-      element: <InventoryPage />,
-    },
+    // {
+    //   path: "/inventory",
+    //   element: <InventoryPage />,
+    // },
 
+    // {
+    //   path: "/appointments",
+    //   element: <Appointments />,
+    // },
+    // {
+    //   path: "/patient/:id",
+    //   element: <PatientDetails />
+    // },
+    // {
+    //   path: "/patient/:id/medical-history",
+    //   element: <MedicalHistory />
+    // },
+    // {
+    //   path: "/patient/:id/test-report",
+    //   element: <TestReport />
+    // },
+    // {
+    //   path:"/doctor-appointments",
+    //   element: <DoctorAppointments />
+    // },
     {
-      path: "/appointments",
-      element: <Appointments />,
-    },
-    {
-      path: "/patient/:id",
-      element: <PatientDetails />
-    },
-    {
-      path: "/patient/:id/medical-history",
-      element: <MedicalHistory />
-    },
-    {
-      path: "/patient/:id/test-report",
-      element: <TestReport />
-    },
-    {
-      path:"/doctor-appointments",
-      element: <DoctorAppointments />
+      path:"/privacy-policy",
+      element: <PrivacyPolicy/>
     },
 
     {
