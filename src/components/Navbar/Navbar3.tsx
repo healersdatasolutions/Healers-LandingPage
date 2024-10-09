cfa8062042ccc058a84ba57f59578fd992cdd4da
healersdatasolutions <166136254+healersdatasolutions@users.noreply.github.com>
2024-10-09 19:29:13 +0530

    Update Navbar3.tsx

--- src/components/Navbar/Navbar3.tsx
+++ src/components/Navbar/Navbar3.tsx
@@ -136,6 +136,13 @@ export function NavigationMenuNew() {
             </NavigationMenuItem>
             <NavigationMenuItem>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                <Link to="/privacy-policy">
+                Privacy Policy
+                </Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                 <ScrollTo to="contactUs">Contact Us</ScrollTo>
               </NavigationMenuLink>
             </NavigationMenuItem>
@@ -193,6 +200,9 @@ export function NavigationMenuNew() {
               <ScrollTo to="faq" >
                 FAQ
               </ScrollTo>
+              <Link to="/privacy-policy">
+                Privacy Policy
+              </Link>
               <ScrollTo to="contactUs">
                 Contact Us
               </ScrollTo>
@@ -204,7 +214,7 @@ export function NavigationMenuNew() {
       <div className="connectWalletSection flex justify-between items-center rounded-full gap-5">
         <ModeToggle />
         <MetaMaskLogo />
-        <Link to="/login-signup">
+        <Link to="https://gyoj3-uaaaa-aaaap-qkfra-cai.icp0.io/login ">
           <button className="relative inline-flex h-12 w-36 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 transform hover:-translate-y-1 transition duration-400">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
             <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
