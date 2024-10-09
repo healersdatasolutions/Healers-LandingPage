54b0ddc681e3a5c605b49233d086caf6cfe29afa
healersdatasolutions <166136254+healersdatasolutions@users.noreply.github.com>
2024-10-09 19:28:25 +0530

    Update LandingPage.jsx

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 'use client'
 
-import React, { useRef, useState, useEffect } from 'react'
-import { useInView } from 'react-intersection-observer'
+// import React, { useRef, useState, useEffect } from 'react'
+// import { useInView } from 'react-intersection-observer'
 import Hero from '../../components/HeroSection/Hero'
 // import { CardSpotlightEffect1 } from '../../components/CardSpotlightEffect'
 import { NavigationMenuNew } from '../../components/Navbar/Navbar3'
