ad0df07f5e0156ce53cefb160ab5340fd6109f44
healersdatasolutions <166136254+healersdatasolutions@users.noreply.github.com>
2024-10-11 21:47:08 +0530

    Update Navbar3.tsx

--- src/components/Navbar/Navbar3.tsx
+++ src/components/Navbar/Navbar3.tsx
@@ -213,7 +213,7 @@ export function NavigationMenuNew() {
 
       <div className="connectWalletSection flex justify-between items-center rounded-full gap-5">
         <ModeToggle />
-        <MetaMaskLogo />
+      //  <MetaMaskLogo />
         <Link to="https://gyoj3-uaaaa-aaaap-qkfra-cai.icp0.io/login ">
           <button className="relative inline-flex h-12 w-36 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 transform hover:-translate-y-1 transition duration-400">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
